535433891b72f8e6587bfa69ec72fd8ebbfdce77
Michael Staib <michael@chillicream.com>
2019-12-10 14:49:54 +0000

    prepared client demp

--- london_canary_wharf_usergroup_2019/7_client/Program.cs
+++ london_canary_wharf_usergroup_2019/7_client/Program.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
+using StrawberryShake.Transport.WebSockets;
 
 namespace StarWarsClientDemo
 {
@@ -9,17 +10,7 @@ namespace StarWarsClientDemo
     {
         static async Task Main(string[] args)
         {
-            var serviceCollection = new ServiceCollection();
-            serviceCollection.AddHttpClient(
-                "StarWarsClient",
-                c => c.BaseAddress = new Uri("http://localhost:5000/graphql"));
-            serviceCollection.AddStarWarsClient();
 
-            IServiceProvider services = serviceCollection.BuildServiceProvider();
-            var client = services.GetRequiredService<IStarWarsClient>();
-            var result = await client.GetHeroAsync(Episode.NewHope);
-            result.EnsureNoErrors();
-            Console.WriteLine(result.Data?.Hero?.Name);
         }
     }
 }
