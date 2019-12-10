d87282b29c486532f901640c5afe006c4ca67a02
Michael Staib <michael@chillicream.com>
2019-12-10 12:14:49 +0000

    fixed first two demos

---
+++ london_canary_wharf_usergroup_2019/7_client/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace StarWarsClientDemo
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddHttpClient(
+                "StarWarsClient",
+                c => c.BaseAddress = new Uri("http://localhost:5000/graphql"));
+            serviceCollection.AddStarWarsClient();
+
+            IServiceProvider services = serviceCollection.BuildServiceProvider();
+            var client = services.GetRequiredService<IStarWarsClient>();
+            var result = await client.GetHeroAsync(Episode.NewHope);
+            result.EnsureNoErrors();
+            Console.WriteLine(result.Data?.Hero?.Name);
+        }
+    }
+}
---
+++ london_canary_wharf_usergroup_2019/8_testing/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Threading.Tasks;
+using HotChocolate;
+using HotChocolate.Execution;
+using HotChocolate.Types;
+using Snapshooter.Xunit;
+using Xunit;
+
+namespace Testing
+{
+    public class MyTests
+    {
+
+    }
+}
