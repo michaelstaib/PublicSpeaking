8eb925a6a27d622a58fecc88f1b11caebf6831bb
Michael Staib <michael@chillicream.com>
2019-12-18 21:32:41 +0100

    updated demos

---
+++ zurich_hsr_2019/4_twitter/Models/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MongoDB.Bson;
+
+namespace Twitter
+{
+    public class Message
+    {
+        public ObjectId Id { get; set; }
+        public string Text { get; set; }
+        public DateTimeOffset Created { get; set; }
+        public int Likes { get; set; }
+        public ObjectId UserId { get; set; }
+        public ObjectId? ReplyToId { get; set; }
+    }
+}
--- zurich_hsr_2019/5_dataloader/UserInput.cs
+++
@@ -1,8 +0,0 @@
-namespace HotChocolate.Examples.Paging
-{
-    public class UserInput
-    {
-        public string Name { get; set; }
-        public string Country { get; set; }
-    }
-}
