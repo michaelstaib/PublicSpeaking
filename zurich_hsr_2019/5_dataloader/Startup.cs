8399dadee573e08e1765b3911b3924f1563f8c03
Michael Staib <michael@chillicream.com>
2019-12-18 15:04:49 +0100

    Added base demos

---
+++ zurich_hsr_2019/5_dataloader/Startup.cs
@@ -0,0 +1,45 @@
+﻿
+using HotChocolate.AspNetCore;
+using HotChocolate.Utilities;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace HotChocolate.Examples.Paging
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // setup type conversion for object id
+            TypeConversion.Default.Register<string, ObjectId>(from => ObjectId.Parse(from));
+            TypeConversion.Default.Register<ObjectId, string>(from => from.ToString());
+
+            // setup the repositories
+            services.AddSingleton<IMongoClient>(new MongoClient("mongodb://127.0.0.1:27017"));
+            services.AddSingleton<IMongoDatabase>(s => s.GetRequiredService<IMongoClient>().GetDatabase("PagingDemo"));
+            services.AddSingleton<IMongoCollection<Message>>(s => s.GetRequiredService<IMongoDatabase>().GetCollection<Message>("messages"));
+            services.AddSingleton<IMongoCollection<User>>(s => s.GetRequiredService<IMongoDatabase>().GetCollection<User>("users"));
+            services.AddSingleton<MessageRepository>();
+            services.AddSingleton<UserRepository>();
+
+            // this enables you to use DataLoader in your resolvers.
+            services.AddDataLoaderRegistry();
+
+            // Add GraphQL Services
+            services.AddGraphQL(SchemaBuilder.New()
+                .AddQueryType<QueryType>()
+                .AddMutationType<MutationType>());
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseGraphQL();
+        }
+    }
+}
---
+++ zurich_hsr_2019/7_client/StarWars/Generated/OnReviewResultParser.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.Json;
+using StrawberryShake;
+using StrawberryShake.Configuration;
+using StrawberryShake.Http;
+using StrawberryShake.Http.Subscriptions;
+using StrawberryShake.Transport;
+
+namespace StarWarsClientDemo
+{
+    [System.CodeDom.Compiler.GeneratedCode("StrawberryShake", "11.0.0")]
+    public class OnReviewResultParser
+        : JsonResultParserBase<IOnReview>
+    {
+        private readonly IValueSerializer _intSerializer;
+
+        public OnReviewResultParser(IValueSerializerCollection serializerResolver)
+        {
+            if (serializerResolver is null)
+            {
+                throw new ArgumentNullException(nameof(serializerResolver));
+            }
+            _intSerializer = serializerResolver.Get("Int");
+        }
+
+        protected override IOnReview ParserData(JsonElement data)
+        {
+            return new OnReview1
+            (
+                ParseOnReviewOnReview(data, "onReview")
+            );
+
+        }
+
+        private IReview ParseOnReviewOnReview(
+            JsonElement parent,
+            string field)
+        {
+            JsonElement obj = parent.GetProperty(field);
+
+            return new Review
+            (
+                DeserializeInt(obj, "stars")
+            );
+        }
+
+        private int DeserializeInt(JsonElement obj, string fieldName)
+        {
+            JsonElement value = obj.GetProperty(fieldName);
+            return (int)_intSerializer.Deserialize(value.GetInt32())!;
+        }
+    }
+}
