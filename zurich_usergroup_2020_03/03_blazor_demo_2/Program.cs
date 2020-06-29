60135ac8ee30d6dac7cc33d7f54b0623da3f96b4
Michael Staib <michael@chillicream.com>
2020-06-29 17:08:44 +0200

    prepared demo

--- zurich_usergroup_2020_03/03_blazor_demo_2/Program.cs
+++ zurich_usergroup_2020_03/03_blazor_demo_2/Program.cs
@@ -9,7 +9,7 @@ namespace Client
 {
     public class Program
     {
-        private static readonly string _token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6Im1pY2hhZWxAY2hpbGxpY3JlYW0uY29tIiwiZW1haWwiOiJtaWNoYWVsQGNoaWxsaWNyZWFtLmNvbSIsIkNoYXQuVXNlcklkIjoiODA4YTE1MGYtNDliOC00MjFhLTgxNGEtZmRkMjlmMzYxZTg2IiwibmJmIjoxNTgyOTA1MjUzLCJleHAiOjE1ODI5NDg0NTMsImlhdCI6MTU4MjkwNTI1M30.9-_slvR8bwnWjtbxAMg7vZuU9gRT68TEvQosV293SC4";
+        private static readonly string _token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6Im1pY2hhZWxAY2hpbGxpY3JlYW0uY29tIiwiZW1haWwiOiJtaWNoYWVsQGNoaWxsaWNyZWFtLmNvbSIsIkNoYXQuVXNlcklkIjoiODA4YTE1MGYtNDliOC00MjFhLTgxNGEtZmRkMjlmMzYxZTg2IiwibmJmIjoxNTkzNDQzMTE4LCJleHAiOjE1OTM0ODYzMTgsImlhdCI6MTU5MzQ0MzExOH0.YHQ3FT_eUZlazk-Iurd5y0Bkpt0-URk1_6sQvkhKTug";
 
         public static async Task Main(string[] args)
         {
@@ -19,7 +19,7 @@ namespace Client
                 "ChatClient",
                 (services, client) =>
                 {
-                    client.BaseAddress = new Uri("http://localhost:5000/");
+                    client.BaseAddress = new Uri("https://hotchocolate-chat.azurewebsites.net");
                     client.DefaultRequestHeaders.Authorization =
                         new AuthenticationHeaderValue("bearer", _token);
                 });
