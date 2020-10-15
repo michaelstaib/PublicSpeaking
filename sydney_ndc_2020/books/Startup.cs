8947996a89b4c58f44df81adcbe7e463f6c39886
Michael Staib <michael@chillicream.com>
2020-10-15 21:12:40 +0200

    prototype

---
+++ sydney_ndc_2020/books/Startup.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Threading.Tasks;
+using Demo.Data;
+using Demo.Types;
+using HotChocolate;
+using HotChocolate.Data.Filters;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Demo
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services
+                .AddPooledDbContextFactory<BookContext>(
+                    c => c.UseSqlite("Data Source=books.db"))
+                .AddGraphQLServer()
+                .AddQueryType<Query>()
+                .AddMutationType<Mutation>()
+                .AddSubscriptionType<Subscription>()
+                .AddType<AuthorType>()
+                .AddType<BookType>()
+                .AddFiltering()
+                .AddSorting()
+                .AddInMemorySubscriptions();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseWebSockets();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGraphQL();
+            });
+        }
+    }
+}
