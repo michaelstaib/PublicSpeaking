fb843a746e89c79a824d20bbb28afd97a95f00ce
Michael Staib <michael@chillicream.com>
2020-10-18 23:10:56 +0200

    demo 2

---
+++ channel9_2020/demo2/Query.cs
@@ -0,0 +1,17 @@
+using System.Linq;
+using Demo.Data;
+using HotChocolate;
+using HotChocolate.Data;
+using HotChocolate.Types;
+
+namespace Demo
+{
+    public class Query 
+    {
+        public Book GetBook() => new Book 
+        { 
+            Title = "C# in Depth", 
+            Author = new Author { Name = "Jon Skeet" } 
+        };
+    }
+}
