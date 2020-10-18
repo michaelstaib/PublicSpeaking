b1bdab0eebcefb23065c68c3fa44b031212d7e34
Michael Staib <michael@chillicream.com>
2020-10-18 20:23:13 +0200

    Added demo 1

---
+++ channel9_2020/demo/Copy/Data/BookContext.cs
@@ -0,0 +1,39 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Demo.Data
+{
+    public class BookContext : DbContext
+    {
+        public BookContext()
+        {
+        }
+
+        public BookContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; } = default!;
+
+        public DbSet<Author> Authors { get; set; } = default!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlite("Data Source=demo.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Author>()
+                .HasMany(t => t.Books)
+                .WithOne(t => t.Author!)
+                .HasForeignKey(t => t.AuthorId);
+
+            modelBuilder
+                .Entity<Book>()
+                .HasOne(t => t.Author)
+                .WithMany(t => t!.Books)
+                .HasForeignKey(t => t.AuthorId);
+        }
+    }
+}
---
+++ channel9_2020/demo/Demo/Startup.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Demo.Data;
+using HotChocolate;
+using HotChocolate.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Demo
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services
+                .AddPooledDbContextFactory<BookContext>(
+                    o => o.UseSqlite("Data Source=demo.db"))
+                .AddGraphQLServer()
+                .AddQueryType<Query>()
+                .AddFiltering()
+                .AddSorting();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGraphQL();
+            });
+        }
+    }
+
+}
