e46234c5ecd63df22403dbbda884123fe3d3fa15
Michael Staib <michael@chillicream.com>
2020-11-12 08:35:09 +0100

    Added more snippets

--- dotnetconf_2020/Program.cs
+++ dotnetconf_2020/Program.cs
@@ -9,8 +9,6 @@ namespace Client
 {
     public class Program
     {
-        private static readonly string _token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6Im1pY2hhZWxAY2hpbGxpY3JlYW0uY29tIiwiZW1haWwiOiJtaWNoYWVsQGNoaWxsaWNyZWFtLmNvbSIsIkNoYXQuVXNlcklkIjoiODA4YTE1MGYtNDliOC00MjFhLTgxNGEtZmRkMjlmMzYxZTg2IiwibmJmIjoxNjA1MDQyMzAxLCJleHAiOjE2MDUwODU1MDEsImlhdCI6MTYwNTA0MjMwMX0.3Lo8hp-wcR-0CmsKArndu4dUWdzyR0KxQ8Svz5JiOyQ";
-
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
