534b205e9f3949f026eb1fe2be7eadd2ba5b5ace
Michael Staib <michael@chillicream.com>
2020-11-24 11:51:11 +0100

    Fixed subscription templates

--- cologne_ddc_2020/4_subscriptions/Startup.cs
+++ cologne_ddc_2020/4_subscriptions/Startup.cs
@@ -5,9 +5,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using HotChocolate;
-using HotChocolate.AspNetCore;
-using HotChocolate.Subscriptions;
 using HotChocolate.Types;
 
 
@@ -19,11 +16,9 @@ namespace Subscriptions
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddInMemorySubscriptions();
-
-            services.AddGraphQL(
-                SchemaBuilder.New()
-                    .AddQueryType(c => c.Name("Query").Field("some").Resolver("value")));
+            services
+                .AddGraphQLServer()
+                .AddQueryType(c => c.Name("Query").Field("some").Resolver("value"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,10 +33,10 @@ namespace Subscriptions
 
             app.UseWebSockets();
 
-            app.UseGraphQL();
-
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapGraphQL();
+
                 endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync("Hello World!");
