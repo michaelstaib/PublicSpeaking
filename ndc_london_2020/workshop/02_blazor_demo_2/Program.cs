701deff859a1ce7cc8bdd977f883ed62b33dae93
Michael Staib <michael@chillicream.com>
2021-01-26 14:19:58 +0100

    prepared demo

---
+++ ndc_london_2020/workshop/02_blazor_demo_2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using StrawberryShake.Transport.WebSockets;
+
+namespace Client
+{
+    public class Program
+    {
+        private static readonly string _token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6Im1pY2hhZWxAY2hpbGxpY3JlYW0uY29tIiwiZW1haWwiOiJtaWNoYWVsQGNoaWxsaWNyZWFtLmNvbSIsIkNoYXQuVXNlcklkIjoiODA4YTE1MGYtNDliOC00MjFhLTgxNGEtZmRkMjlmMzYxZTg2IiwibmJmIjoxNjExNjY3MTQ3LCJleHAiOjE2MTE3MTAzNDcsImlhdCI6MTYxMTY2NzE0N30.C2luH-NT_y2J6Sx09o8OfGJr9tsboziaDP0PXon6cdg";
+
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+            builder.Services.AddHttpClient(
+                "ChatClient",
+                (services, client) =>
+                {
+                    client.BaseAddress = new Uri("https://hotchocolate-chat.azurewebsites.net");
+                    client.DefaultRequestHeaders.Authorization =
+                        new AuthenticationHeaderValue("bearer", _token);
+                });
+
+            builder.Services.AddChatClient();
+
+            builder.RootComponents.Add<App>("app");
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
