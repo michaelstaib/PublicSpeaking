64afbfd1af5bc9d719b9297b916c22439d336526
Michael Staib <michael@chillicream.com>
2021-02-03 21:33:25 +0100

    Added demos

---
+++ sydney_blazor_ug_2021/demo2/Generated/ChatClient.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using StrawberryShake;
+
+namespace Client
+{
+    [System.CodeDom.Compiler.GeneratedCode("StrawberryShake", "11.0.0")]
+    public partial class ChatClient
+        : IChatClient
+    {
+        private const string _clientName = "ChatClient";
+
+        private readonly global::StrawberryShake.IOperationExecutor _executor;
+
+        public ChatClient(global::StrawberryShake.IOperationExecutorPool executorPool)
+        {
+            _executor = executorPool.CreateExecutor(_clientName);
+        }
+
+        public global::System.Threading.Tasks.Task<global::StrawberryShake.IOperationResult<global::Client.IGetPeople>> GetPeopleAsync(
+            global::System.Threading.CancellationToken cancellationToken = default)
+        {
+
+            return _executor.ExecuteAsync(
+                new GetPeopleOperation(),
+                cancellationToken);
+        }
+
+        public global::System.Threading.Tasks.Task<global::StrawberryShake.IOperationResult<global::Client.IGetPeople>> GetPeopleAsync(
+            GetPeopleOperation operation,
+            global::System.Threading.CancellationToken cancellationToken = default)
+        {
+            if (operation is null)
+            {
+                throw new ArgumentNullException(nameof(operation));
+            }
+
+            return _executor.ExecuteAsync(operation, cancellationToken);
+        }
+    }
+}
---
+++ sydney_blazor_ug_2021/demo2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Blazor.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using StrawberryShake.Transport.WebSockets;
+
+namespace Client
+{
+    public class Program
+    {
+        private static readonly string _token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6Im1pY2hhZWxAY2hpbGxpY3JlYW0uY29tIiwiZW1haWwiOiJtaWNoYWVsQGNoaWxsaWNyZWFtLmNvbSIsIkNoYXQuVXNlcklkIjoiODA4YTE1MGYtNDliOC00MjFhLTgxNGEtZmRkMjlmMzYxZTg2IiwibmJmIjoxNjEyMzg0MzE1LCJleHAiOjE2MTI0Mjc1MTUsImlhdCI6MTYxMjM4NDMxNX0.HekJKxNW84arlARLQfFnrBlWmXbvh4SaxKQXh9N6DH4";
+
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+            builder.Services.AddHttpClient(
+                "ChatClient",
+                (services, client) =>
+                {
+                    client.BaseAddress = new Uri("https://hotchocolate-chat.azurewebsites.net");
+                    client.DefaultRequestHeaders.Authorization =
+                        new AuthenticationHeaderValue("bearer", _token);
+                });
+
+            builder.Services.AddWebSocketClient(
+                "ChatClient",
+                (service, client) => 
+                {
+                    client.Uri = new Uri("wss://hotchocolate-chat.azurewebsites.net?token=" + _token);
+                });
+
+            builder.Services.AddChatClient();
+
+            builder.RootComponents.Add<App>("app");
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
