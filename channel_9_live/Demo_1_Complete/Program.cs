8ab93d031b445128308f9a2318c69e7cb2367e2f
Michael Staib <michael@chillicream.com>
2021-02-19 17:03:10 +0100

    updated token

--- channel_9_live/Demo_1_Complete/Program.cs
+++ channel_9_live/Demo_1_Complete/Program.cs
@@ -13,7 +13,7 @@ namespace Demo
 {
     public class Program
     {
-        private const string _token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6Im1pY2hhZWxAY2hpbGxpY3JlYW0uY29tIiwiZW1haWwiOiJtaWNoYWVsQGNoaWxsaWNyZWFtLmNvbSIsIkNoYXQuVXNlcklkIjoiODA4YTE1MGYtNDliOC00MjFhLTgxNGEtZmRkMjlmMzYxZTg2IiwibmJmIjoxNjEzNjY2OTI2LCJleHAiOjE2MTM3MTAxMjYsImlhdCI6MTYxMzY2NjkyNn0.Q6ak7Wr_QFfq4Kg1SKYh_mIXrTz6MrBG6c_YrHCDRP4";
+        private const string _token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6Im1pY2hhZWxAY2hpbGxpY3JlYW0uY29tIiwiZW1haWwiOiJtaWNoYWVsQGNoaWxsaWNyZWFtLmNvbSIsIkNoYXQuVXNlcklkIjoiODA4YTE1MGYtNDliOC00MjFhLTgxNGEtZmRkMjlmMzYxZTg2IiwibmJmIjoxNjEzNzUwNTUxLCJleHAiOjE2MTM3OTM3NTEsImlhdCI6MTYxMzc1MDU1MX0.4UK1lQjWnE6bPLhiuHuKeVpQRTzqB_qgnrDGAjv6odc";
         
         public static async Task Main(string[] args)
         {
