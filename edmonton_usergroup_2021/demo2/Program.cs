692a94a0ef07fe9b903deaf866decf32ffc352bb
Michael Staib <michael@chillicream.com>
2021-04-06 18:40:55 +0200

    prepared demos

---
+++ edmonton_usergroup_2021/demo2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var services = CreateServices();
+
+
+            Console.WriteLine("Hit enter to close!");
+            Console.ReadLine();
+        }
+
+        private static ServiceProvider CreateServices()
+        {
+            var serviceCollection = new ServiceCollection();
+
+            // configuration goes here ...
+
+            return serviceCollection.BuildServiceProvider();
+        }
+    }
+}
