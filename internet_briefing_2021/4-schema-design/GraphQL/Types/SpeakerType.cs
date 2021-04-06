3c35d537eaf13f216880c96a9a4e50bd4168e208
Michael Staib <michael@chillicream.com>
2021-04-06 18:20:00 +0200

    Added demos

---
+++ internet_briefing_2021/4-schema-design/GraphQL/Types/SpeakerType.cs
@@ -0,0 +1,56 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ConferencePlanner.GraphQL.Data;
+using ConferencePlanner.GraphQL.DataLoader;
+using HotChocolate;
+using HotChocolate.Resolvers;
+using HotChocolate.Types;
+
+namespace ConferencePlanner.GraphQL.Types
+{
+    public class SpeakerType : ObjectType<Speaker>
+    {
+        protected override void Configure(IObjectTypeDescriptor<Speaker> descriptor)
+        {
+            descriptor
+                .ImplementsNode()
+                .IdField(t => t.Id)
+                .ResolveNodeWith<SpeakerResolvers>(t => t.GetSpeakerByIdAsync(default, default!, default));
+
+            descriptor
+                .Field(t => t.SessionSpeakers)
+                .ResolveWith<SpeakerResolvers>(t => t.GetSessionsAsync(default!, default!, default!, default))
+                .UseDbContext<ApplicationDbContext>()
+                .Name("sessions");
+        }
+
+        private class SpeakerResolvers
+        {
+            public async Task<IEnumerable<Session>> GetSessionsAsync(
+                Speaker speaker,
+                [ScopedService] ApplicationDbContext dbContext,
+                SessionByIdDataLoader sessionById,
+                CancellationToken cancellationToken)
+            {
+                await Task.Delay(600);
+
+                int[] speakerIds = await dbContext.Speakers
+                    .Where(s => s.Id == speaker.Id)
+                    .Include(s => s.SessionSpeakers)
+                    .SelectMany(s => s.SessionSpeakers.Select(t => t.SessionId))
+                    .ToArrayAsync();
+
+                return await sessionById.LoadAsync(speakerIds, cancellationToken);
+            }
+
+            public Task<Speaker> GetSpeakerByIdAsync(
+                int id, 
+                SpeakerByIdDataLoader speakerById,
+                CancellationToken cancellationToken) =>
+                speakerById.LoadAsync(id, cancellationToken);
+        }
+    }
+}
