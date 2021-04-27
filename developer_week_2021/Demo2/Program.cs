cc286965f1514c5793a660e6f11b171fb9b60a57
Michael Staib <michael@chillicream.com>
2021-04-27 14:02:09 +0200

    Demo Complete

---
+++ developer_week_2021/Demo2/Program.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Demo2
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+
+            builder.Services
+                .AddConferenceClient()
+                .ConfigureHttpClient(client => client.BaseAddress = new Uri("https://hc-conference-app.azurewebsites.net/graphql/"))
+                .ConfigureWebSocketClient(client => client.Uri = new Uri("wss://hc-conference-app.azurewebsites.net/graphql/"));
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
