6d57c5a42399a311fc5c2a4359c4d35b28d6c219
Michael Staib <michael@chillicream.com>
2021-04-27 14:12:12 +0200

    Renamed

--- developer_week_2021/Demo2/Program.cs
+++ developer_week_2021/Demo2/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace Demo2
+namespace Demo
 {
     public class Program
     {
