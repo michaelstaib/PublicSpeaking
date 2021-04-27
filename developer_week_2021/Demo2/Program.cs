9af214559b3b12aa683773fac6ad34ba2b8bd159
Michael Staib <michael@chillicream.com>
2021-04-27 14:05:27 +0200

    cleanup

--- developer_week_2021/Demo2/Program.cs
+++ developer_week_2021/Demo2/Program.cs
@@ -17,11 +17,6 @@ namespace Demo2
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services
-                .AddConferenceClient()
-                .ConfigureHttpClient(client => client.BaseAddress = new Uri("https://hc-conference-app.azurewebsites.net/graphql/"))
-                .ConfigureWebSocketClient(client => client.Uri = new Uri("wss://hc-conference-app.azurewebsites.net/graphql/"));
-
             await builder.Build().RunAsync();
         }
     }
