6c9c4961723374d86b9b4b8e38568a24f2ed2535
Michael Staib <michael@chillicream.com>
2021-08-11 17:25:11 +0200

    prepared demos

---
+++ scottish_devs_2021/demo3/Mutation.cs
@@ -0,0 +1,47 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Demo.Data;
+
+namespace Demo
+{
+    public class Mutation
+    {
+        private readonly BookContext _bookContext;
+
+        public Mutation(BookContext bookContext)
+        {
+            _bookContext = bookContext;
+        }
+
+        public async Task<AddAuthorPayload> AddAuthorAsync(
+            AddAuthorInput input,
+            CancellationToken cancellationToken)
+        {
+            var author = new Author 
+            {
+                Name = input.Name
+            };
+
+            _bookContext.Authors.Add(author);
+            await _bookContext.SaveChangesAsync(cancellationToken);
+
+            return new AddAuthorPayload(author);
+        }
+
+        public async Task<AddBookPayload> AddBookAsync(
+            AddBookInput input,
+            CancellationToken cancellationToken)
+        {
+            var book = new Book 
+            {
+                Title = input.Title,
+                AuthorId = input.AuthorId
+            };
+
+            _bookContext.Books.Add(book);
+            await _bookContext.SaveChangesAsync(cancellationToken);
+
+            return new AddBookPayload(book);
+        }
+    }
+}
